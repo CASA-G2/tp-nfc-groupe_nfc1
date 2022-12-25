028f9430f4c4afe6021b8775767e17ed2947be5c
amine-talib <bonsoir.aminos@gmail.com>
2022-12-25 22:04:50 +0100

    projet nfc g2

--- app/src/main/java/org/mbds/nfctag/Formular.java
+++ app/src/main/java/org/mbds/nfctag/Formular.java
@@ -1,14 +1,75 @@
 package org.mbds.nfctag;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import org.mbds.nfctag.databinding.ActivityMainBinding;
+import org.mbds.nfctag.read.NFCReaderActivity;
+import org.mbds.nfctag.write.NFCWriterActivity;
 
 import java.security.PrivateKey;
 
-public class Formular extends AppCompatActivity {
+
+public class Formular extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private ActivityMainBinding binding;
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formular);
+        spinner = findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.location, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        handleActions();
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    private void handleActions() {
+        binding.btnReadTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Formular.this, NFCReaderActivity.class));
+            }
+        });
+
+        binding.btnWriteTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Formular.this, NFCWriterActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String choice = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(getApplicationContext(), choice, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
